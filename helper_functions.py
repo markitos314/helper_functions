a17f89f0b61cf8d0fa14c101d6bd891678bedc62
markitos314 <44953005+markitos314@users.noreply.github.com>
2021-06-29 10:38:36 +0100

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,16 +1,16 @@
 # Create an early stopping callback
 
 import tensorflow as tf
-def early_stopping(monitor='val_loss', min_delta=0.05, patience=3, verbose=1, mode=auto, restore_best_weights=True):
+def early_stopping(monitor='val_loss', min_delta=0.05, patience=3, verbose=1, mode='auto', restore_best_weights=True):
   """
   Creates an early_stopping element to be then used in the callback section when fitting a model.
   """
-  early_stopping = tf.keras.callbacks.EarlyStopping(monitor,
-                                                    min_delta,
-                                                    patience,
-                                                    verbose, 
-                                                    mode,
-                                                    restore_best_weights)
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor=monitor,
+                                                    min_delta=min_delta,
+                                                    patience=patience,
+                                                    verbose=verbose, 
+                                                    mode=mode,
+                                                    restore_best_weights=restore_best_weights)
   return early_stopping
 
 # Unzip .zip file
